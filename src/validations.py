2564d2efc880c4c06776ce9c912c76333fc663f6
A. Flaxman <abie@uw.edu>
2011-11-21 22:37:32 -0800

    refactor and add failing validation of mis-specified model

--- src/validations.py
+++ src/validations.py
@@ -12,37 +12,49 @@ reload(data)
 reload(models)
 reload(graphics)
 
-def validate_simple_model_once(n=[33, 21, 22, 22, 24, 11]):
+def good_simple_sim(n=[33, 21, 22, 22, 24, 11]):
     # generate data and model
     d = data.simple_hierarchical_data(n)
     m = models.simple_hierarchical_model(d['y'])
 
     # fit model with MCMC
-    #mc.MCMC(m).sample(20000, 10000, 2)
-    mc.MCMC(m).sample(5000)
+    mc.MCMC(m).sample(6000, 1000)
 
-    # tally posterior quantiles
-    results = {}
-    
-    for var in 'inv_sigma_sq mu inv_tau_sq mu_by_tau alpha_bar alpha_bar_by_sigma'.split():
-        stats = m[var].stats()
-        results[var] = [(d[var] > m[var].trace()).sum() / float(stats['n'])]
-
-    stats = m['alpha'].stats()
-    for j, alpha_j in enumerate(d['alpha']):
-        results['alpha_%d'%j] = [(alpha_j > m['alpha'].trace()[:,j]).sum() / float(stats['n'])]
-
-    stats = m['alpha_by_sigma'].stats()
-    for j, alpha_j_by_sigma in enumerate(d['alpha_by_sigma']):
-        results['alpha_%d_by_sigma'%j] = [(alpha_j_by_sigma > m['alpha_by_sigma'].trace()[:,j]).sum() / float(stats['n'])]
-
-    return results
+    return d, m
 
 
-def validate_simple_model(N_rep=20):
+def bad_simple_sim(n=[33, 21, 22, 22, 24, 11]):
+    # generate data and model
+    d = data.simple_hierarchical_data(n)
+    m = models.simple_hierarchical_model(d['y'])
+    m['mu'].parents['mu'] = -5.
+    # fit model with MCMC
+    mc.MCMC(m).sample(6000, 1000)
+
+    return d, m
+
+
+def validate_simple_model(N_rep=20, simulation=good_simple_sim):
     q = pandas.DataFrame()
     for n in range(N_rep):
-        results = validate_simple_model_once()
+        # simulate data and fit model
+        d, m = simulation()
+
+        # tally posterior quantiles
+        results = {}
+
+        for var in 'inv_sigma_sq mu inv_tau_sq mu_by_tau alpha_bar alpha_bar_by_sigma'.split():
+            stats = m[var].stats()
+            results[var] = [(d[var] > m[var].trace()).sum() / float(stats['n'])]
+
+        stats = m['alpha'].stats()
+        for j, alpha_j in enumerate(d['alpha']):
+            results['alpha_%d'%j] = [(alpha_j > m['alpha'].trace()[:,j]).sum() / float(stats['n'])]
+
+        stats = m['alpha_by_sigma'].stats()
+        for j, alpha_j_by_sigma in enumerate(d['alpha_by_sigma']):
+            results['alpha_%d_by_sigma'%j] = [(alpha_j_by_sigma > m['alpha_by_sigma'].trace()[:,j]).sum() / float(stats['n'])]
+
         q = q.append(pandas.DataFrame(results, index=['q_rep_%d'%n]))
 
     z = {}
